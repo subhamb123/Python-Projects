af391dff29c5eb726ecd8a2435a1d26ed5fd048e
Subham Behera <subham.behera21@gmail.com>
2020-04-09 17:15:04 -0700

    Create Matching Lists 3.py

---
+++ Level 1/Matching Lists 3.py
@@ -0,0 +1,12 @@
+baby_names = ["Emma", "Olivia", "Noah", "Liam", "Ava", "Sophia", "William", "Mason", "James", "Isabella"]
+baby_numbers = [19414, 19246, 19015, 18138, 16237, 16070, 15668, 15192, 14776, 14722]
+
+print("Here are the most popular baby names of the year 2016,")
+print("with how many babies were named each.")
+
+for i in range(len(baby_names)):
+    output = str(i+1)
+    output += ") " + baby_names[i]
+    output += " - "
+    output += str(baby_numbers[i])
+    print(output)
