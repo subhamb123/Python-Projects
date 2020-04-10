c954db09ae8a6b862174102dd2301467c12d456b
Subham Behera <subham.behera21@gmail.com>
2020-04-09 17:18:28 -0700

    Create For Range.py

---
+++ Level 1/For Range.py
@@ -0,0 +1,4 @@
+books = ["Good Omens", "Harry Potter Series", "Catch 22", "Enchantress from the Stars", "Cloud Atlas", "Left Hand of Darkness", "Ender's Game", "The Raven Cycle", "The Hunger Games", "Dragonriders of Pern"]
+
+for title in range(len(books)):
+    print(str(title+1) + ": " + books[title])
