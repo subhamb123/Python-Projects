51c3001fd6386f088ca392499955dd74fd000291
Subham Behera <subham.behera21@gmail.com>
2020-04-09 17:27:25 -0700

    Create Count Items 3.py

---
+++ Level 1/Count Items 3.py
@@ -0,0 +1,8 @@
+numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+
+three_letters = 0
+for number in numbers:
+    if len(number) == 3:
+        three_letters += 1
+
+print(str(three_letters) + " of the numbers from 1 to 10 are written with three letters.")
