011c3d25bcc6a7e92bb46ebc95cdc25012e42319
Subham Behera <subham.behera21@gmail.com>
2020-04-14 17:13:31 -0700

    Create findMin.py

---
+++ Level 3/findMin.py
@@ -0,0 +1,6 @@
+numbers = [16, 12, 25, 44]
+    min = 100
+    for number in numbers:
+        if number < min:
+            min = number
+    print("The lowest number in the list is " + str(min) + ".")
