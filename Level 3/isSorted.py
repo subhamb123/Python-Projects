344f252df3bace6aa17a975c3e52ce61b60e6ed1
Subham Behera <subham.behera21@gmail.com>
2020-04-14 17:44:30 -0700

    Update isSorted.py

--- Level 3/isSorted.py
+++ Level 3/isSorted.py
@@ -1,15 +1,15 @@
 switch = True
-    sorted = True
-    while switch:
-        floats = [16.1, 12.3, 22.2, 14.4]
-        if len(floats) == 1:
-            break
-        for number in range(0, len(floats)-1):
-            if floats[number] > floats[number+1]:
-                sorted = False
-                switch = False
-        switch = False
-    if sorted:
-        print("The list is sorted.")
-    else:
-        print("The list isn't sorted.")
+sorted = True
+while switch:
+    floats = [16.1, 12.3, 22.2, 14.4]
+    if len(floats) == 1:
+        break
+    for number in range(0, len(floats)-1):
+        if floats[number] > floats[number+1]:
+            sorted = False
+            switch = False
+    switch = False
+if sorted:
+    print("The list is sorted.")
+else:
+    print("The list isn't sorted.")
