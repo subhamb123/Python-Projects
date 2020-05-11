c1b2da33de3bb3a703b7f91fc2c86d622caa155f
subhamb123 <subham.behera21@gmail.com>
2020-05-11 13:29:00 -0700

    Moved around files.

---
+++ Level 1/Functions.py
@@ -0,0 +1,46 @@
+import random
+import tsk
+import pygame
+pygame.init()
+
+def find_winner():
+    print("Winning Bee")
+    max_x = 0
+    for bee in bees:
+        if bee.x > max_x:
+            max_x = bee.x
+    
+    print("The farthest bee was at " + str(max_x))
+
+w = pygame.display.set_mode([1018, 573])
+c = pygame.time.Clock()
+
+timer = 3000
+bees = []
+background = tsk.Sprite("OutdoorBushes.jpg", 0, 0)
+for i in range(10):
+    bee = tsk.Sprite("Bee.png", 0, 50 + (i * 50))
+    bees.append(bee)
+
+drawing = True
+while drawing:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            drawing = False
+
+    for bee in bees:
+        bee.x += random.randint(1, 20) * .025 * c.get_time()
+
+    background.draw()
+    for bee in bees:
+        bee.draw()
+
+    timer -= c.get_time()
+    pygame.display.flip()
+    c.tick(30)
+
+    if timer <= 0:
+        drawing = False
+
+find_winner()
--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # Python Projects
 
-Here are the python projects I have worked on. The "Level 1" folder contains projects that are basic python skills practice. 
+Here are the python projects I have worked on. The "Level 1" folder contains projects that are basic python skills practice.
 
-Projects that have animations can be found in the "Level 2" folder. Animations are done using the pygame and tsk libraries. 
+Advanced algorithm practice can be found in the "Level 2" folder.  
 
-Advanced algorithm practice can be found in the "Level 3" folder. 
-
-Thank you!
+Projects that have animations can be found in the "Level 3" folder. Animations are done using the pygame and tsk libraries. 
